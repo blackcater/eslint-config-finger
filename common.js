05614920ed6d06db3dc68646c05deebc68469215
blackcater <blackcater2015@gmail.com>
2017-03-15 15:28:39 +0800

    refactoring

---
+++ common.js
@@ -0,0 +1,17 @@
+/**
+ * Basic Configuration, you can use it in any project
+ *
+ *
+ */
+module.exports = {
+  "extends": [
+    "./lib/partial/errors",
+    "./lib/partial/best-practices",
+    "./lib/partial/variables",
+    "./lib/partial/stylistic",
+  ].map(require.resolve),
+
+  "env": { browser: true },
+
+  "rules": {},
+}
--- react.js
+++ react.js
@@ -2,17 +2,9 @@
  * If you use react
  */
 module.exports = {
-  "extends": "eslint:recommended",
-  "ecmaVersion": 6,
-  "ecmaFeatures": {
-    "jsx": true,
-  },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true,
-  },
-  "rules": {
-    "no-console": 1,
-  },
+  "extends": [
+    "./index",
+    "./lib/plugins/react",
+    "./lib/plugins/jsx",
+  ].map(require.resolve),
 }
